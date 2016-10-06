aceb66222297976f697203d21d12c02c136a8df3
Fabricio Godoy <skarllot@gmail.com>
2016-10-05 23:48:21 -0300

    Updated assembly info data

--- TaskParallel/Compact/Properties/AssemblyInfo.cs
+++ TaskParallel/Compact/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TaskParallel (NET35 CE)")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle(Consts.Title)]
+[assembly: AssemblyDescription(Consts.Description)]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TaskParallel Compact Edition")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany(Consts.Company)]
+[assembly: AssemblyProduct(Consts.Product)]
+[assembly: AssemblyCopyright(Consts.Copyright)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(Consts.ComVisible)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fee8e6ad-84e3-4cb0-93ac-a63c25257b0e")]
+[assembly: Guid(Consts.Guid)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,7 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Consts.Version)]
+#if !WindowsCE
+[assembly: AssemblyFileVersion(Consts.Version)]
+#endif
 
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TaskParallel.Tests")]
+[assembly: InternalsVisibleTo("TaskParallel.Tests")]
---
+++ TaskParallel/Consts.cs
@@ -0,0 +1,17 @@
+internal static class Consts
+{
+    public const string Description = "Provides a backport of Task<T> for .NET 3.5 Compact Edition";
+    public const string Company = "Fabrício Godoy";
+    public const string Product = "TaskParallelCE";
+    public const string Copyright = "Copyright © Fabrício Godoy 2016";
+    public const bool ComVisible = false;
+    public const string Version = "1.0.1";
+
+#if WindowsCE
+    public const string Title = "TaskParallel (Compact Edition)";
+    public const string Guid = "fee8e6ad-84e3-4cb0-93ac-a63c25257b0e";
+#else
+    public const string Title = "TaskParallel (Full Profile)";
+    public const string Guid = "d78f0942-7465-44e3-95ec-b60ac01938b8";
+#endif
+}
